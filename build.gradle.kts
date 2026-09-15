f0edf17cdf2566e8cb7aab9ebe5f92adee6abc7a
Dhruuvil <dhruvilthesiya14@gmail.com>
2026-09-15 11:24:06 +0530

    build.gradle.kts
    Updated Android Gradle plugin version and modified SDK versions. Added publishing configuration for Maven.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,32 @@
 plugins {
-    alias(libs.plugins.android.library)
+    id("com.android.library") version "9.4.0"
+    id("org.jetbrains.kotlin.android") version "2.2.10"
+    id("maven-publish")
 }
 
 android {
     namespace = "com.example.mylibrary"
-    compileSdk {
-        version = release(37)
-    }
+    compileSdk = 37
 
     defaultConfig {
-        minSdk = 30
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        minSdk = 23
     }
 
+    publishing {
+        singleVariant("release")
+    }
 }
 
-dependencies {
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.junit)
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.Dhruuvil"
+                artifactId = "MyLibrary"
+                version = "1.0.0"
+            }
+        }
+    }
 }
